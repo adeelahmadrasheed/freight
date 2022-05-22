59e4743243fd8d90a7caf472dd92ff899e896263
adeelahmadrasheed <adeelahmadrasheed@gmail.com>
2022-05-22 18:13:46 +0500

    implemented devise gem
    imp views: login, register, profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,42 +1,44 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root 'page#home'
 
-get 'about', to: 'page#about'
+  root 'page#home'
 
-get 'contact', to: 'page#contact'
+  get 'about', to: 'page#about'
 
-get 'service', to: 'page#service'
+  get 'contact', to: 'page#contact'
 
-get 'blog', to: 'page#blog'
+  get 'service', to: 'page#service'
 
-get 'login', to: 'page#login'
+  get 'blog', to: 'page#blog'
 
-get 'register', to: 'page#register'
+  get 'login', to: 'page#login'
 
-get 'shipping', to: 'page#shipping'
+  get 'register', to: 'page#register'
 
-get 'current_shipment', to: 'page#current_shipment'
+  get 'shipping', to: 'page#shipping'
 
-get 'shipment_history', to: 'page#shipment_history'
+  get 'current_shipment', to: 'page#current_shipment'
 
-get 'shipment_status', to: 'page#shipment_status'
+  get 'shipment_history', to: 'page#shipment_history'
 
-get 'shipment_tracking', to: 'page#shipment_tracking'
+  get 'shipment_status', to: 'page#shipment_status'
 
-get 'shipping', to: 'page#shipping'
+  get 'shipment_tracking', to: 'page#shipment_tracking'
 
-get 'shipping', to: 'page#shipping'
+  get 'shipping', to: 'page#shipping'
 
-get 'shipping', to: 'page#shipping'
+  get 'shipping', to: 'page#shipping'
 
-get 'new_shipping', to: 'page#new_shipping'
+  get 'shipping', to: 'page#shipping'
 
-get 'air_freight', to: 'page#air_freight'
+  get 'new_shipping', to: 'page#new_shipping'
 
-get 'ocean_shipping', to: 'page#ocean_shipping'
+  get 'air_freight', to: 'page#air_freight'
 
-get 'car_shipping', to: 'page#car_shipping'
+  get 'ocean_shipping', to: 'page#ocean_shipping'
+
+  get 'car_shipping', to: 'page#car_shipping'
 
 
 
